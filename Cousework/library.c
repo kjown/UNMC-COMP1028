d9bf8fad4cb6528ded8429fa0e73cfbefa598e3b
root <root@LAPTOP-9OREQCG4>
2023-11-17 16:54:03 +0800

    update

--- Cousework/library.c
+++ Cousework/library.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
 
+// functions to greet user and obtain login details
+void greet();
+void login();
+
+// 
+void mainMenu();
+void searchBooks();
+void borrow_return();
+void display();
+void penalty();
+void monthlyReport();
+
+// struct to store student details
+
+// struct to store book details
+
+
 int main() {
-    printf("")
+    greet();
+
+    return 0;
+}
+
+void greet() {
+    printf("\n\n");
+    printf("\t+====================================+\n");
+    printf("\t|                                    |\n");
+    printf("\t--------------------------------------\n");
+    printf("\t|\tWELCOME TO THE LIBRARY       |\n");
+    printf("\t--------------------------------------\n");
+    printf("\t|                                    |\n");
+    printf("\t+====================================+\n");
+    login();
+}
+
+void login() {
+
 }
